18b219e2f0b0f6011f8f7b378bf70f4ca8dcd23f
Aaron Landis <alspore@gmail.com>
2014-11-17 11:36:10 -0500

    split sorting into functions

--- sort.py
+++ sort.py
@@ -12,18 +12,53 @@ def show(name, img):
 def tohsl(data):
     return colorsys.rgb_to_hls(data[2], data[1], data[0])
 
-level = float(sys.argv[2])
+def checkdown(img, y, x):
+    if abs(tohsl(img[y, x])[2] - tohsl(img[y-1, x])[2]) < level:
+        return True
+    return False
 
+def checkright(img, y, x):
+    if abs(tohsl(img[y, x])[2] - tohsl(img[y, x-1])[2]) < level:
+        return True
+    return False
+
+def down(img):
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            if y != 0:
+                if checkdown(img, y, x):
+                    img[y, x] = img[y-1, x]
+
+        print('{0:.0%}'.format(float(y)/img.shape[0]) + ' complete')
+    print 'done with down pass'
+
+def right(img):
+    for x in range(img.shape[1]):
+        for y in range(img.shape[0]):
+            if x != 0:
+                if checkright(img, y, x):
+                    img[y, x] = img[y, x-1]
+
+        print('{0:.0%}'.format(float(x)/img.shape[1]) + ' complete')
+    print 'done with right pass'
+
+def downright(img):
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            if y != 0:
+                if checkdown(img, y, x):
+                    img[y, x] = img[y-1, x]
+            if x != 0:
+                if checkright(img, y, x):
+                    img[y, x] = img[y, x-1]
+            print('{0:.0%}'.format(float(y + x)/(img.shape[0] + img.shape[1])) + ' complete')
+
+
+level = float(sys.argv[2])
 imgname = sys.argv[1]
 img = cv2.imread('in/' + imgname, -1)
 
-for y in range(img.shape[0]):
-    for x in range(img.shape[1]):
-        if y != 0:
-            if abs(tohsl(img[y, x])[2] - tohsl(img[y-1, x])[2]) < level:
-                img[y, x] = img[y-1, x]
-
-    print('{0:.0%}'.format(float(y)/img.shape[0]) + ' complete')
+downright(img)
 
 show('main', img)
 print 'level : ' + str(level)
